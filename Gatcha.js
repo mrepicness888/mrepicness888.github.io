a4e9e19d9073ca0a1941d4419804f72cbd44bb02
Michael Ha <150257321+mrepicness888@users.noreply.github.com>
2024-11-26 23:16:38 +0000

    initial website

---
+++ Gatcha.js
@@ -0,0 +1,98 @@
+
+async function randomImage(pulls) {
+    const pics = [ 
+        "gatcha-images/2-star-fish-michael.jpg",
+        "gatcha-images/4-star-goofy-grace.jpg",
+        "gatcha-images/5-star-doggy-grace.png",
+        "gatcha-images/5-star-sexy-grace.jpg",
+
+    ]; 
+
+    const twostar = ["gatcha-images/2-star-fish-michael.jpg",
+    "gatcha-images/2-star-real-fishmichael.png",
+    "gatcha-images/2-star-mother-and-son.png"
+    ]
+    const threestar = ["gatcha-images/3-star-sleeping-grace.png",
+      "gatcha-images/3-star-mikey-with-children.png",
+      "gatcha-images/3-star-glasses-grace.png"
+    ]
+    const fourstar = ["gatcha-images/4-star-goofy-grace.jpg",
+      "gatcha-images/4-star-gamer-chicken.png"
+    ]
+    const fivestar = ["gatcha-images/5-star-doggy-grace.png",
+      "gatcha-images/5-star-sexy-grace.jpg"]
+
+
+    document.getElementsByClassName("grid-container")[0].innerHTML='';
+    var parentDIV = document.getElementsByClassName("grid-container")[0];
+    var modal = document.getElementById("myModal");
+
+    for(var i=0;i<pulls;i++){
+        var stararray = []
+        var starnum
+        var star = Math.random()
+        if (star <= 0.5){
+          stararray = twostar
+          starnum = 2
+        }
+        else if (star > 0.5 && star <= 0.8){
+          stararray = threestar
+          starnum = 3
+        }
+        else if (star > 0.8 && star <= 0.98){
+          stararray = fourstar
+          starnum = 4
+        }
+        else {
+          stararray = fivestar
+          starnum = 5
+        }
+        var number = Math.floor(Math.random()*stararray.length); 
+        var tempDIV= document.createElement('div');
+        modal.style.display = "block";
+        tempDIV.setAttribute('class','grid-item');
+        var modalStars = '<div class = stars>'
+
+        for(var s=0;s<starnum;s++){
+          modalStars += '<span style="font-size:300%;color:yellow;">&#9733;</span>'
+        }
+
+        modalStars += '</div>'
+        var innerHTML= '<div class= "pull">'+ modalStars + '<img src="' + stararray[number]+'" /></div>'
+        document.getElementsByClassName("modal-image")[0].innerHTML=innerHTML;
+        tempDIV.innerHTML=innerHTML;
+        parentDIV.appendChild(tempDIV);
+        await sleep(1500)
+    }
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ TrivaStartPage.js
@@ -0,0 +1,75 @@
+const quizData = [
+    {
+      question: "What is the name of Mikeys brother?",
+      options: ["Tiffany", "Alex", "Andon", "Fish Michael"],
+      answer: "Andon"
+    },
+    {
+      question: "How old is Michael?",
+      options: ["18", "19", "20", "21"],
+      answer: "20"
+    },
+    {
+      question: "What’s my favorite season of the year?",
+      options: ["Spring", "Summer", "Autumn", "Winter"],
+      answer: "Autumn"
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Jupiter", "Saturn", "Mars", "Earth"],
+      answer: "Jupiter"
+    },
+    // Add more questions here...
+  ];
+  
+  const questionElement = document.getElementById("question");
+  const optionsElement = document.getElementById("options");
+  const submitButton = document.getElementById("submit");
+  
+  let currentQuestion = 0;
+  let score = 0;
+  
+  function showQuestion() {
+    const question = quizData[currentQuestion];
+    questionElement.innerText = question.question;
+  
+    optionsElement.innerHTML = "";
+    question.options.forEach(option => {
+      const button = document.createElement("button");
+      button.innerText = option;
+      optionsElement.appendChild(button);
+      button.addEventListener("click", selectAnswer);
+    });
+  }
+  
+  function selectAnswer(e) {
+    const selectedButton = e.target;
+    const answer = quizData[currentQuestion].answer;
+  
+    if (selectedButton.innerText === answer) {
+      score++;
+    }
+  
+    currentQuestion++;
+  
+    if (currentQuestion < quizData.length) {
+      showQuestion();
+    } else {
+      showResult();
+    }
+  }
+  
+  function showResult() {
+    quiz.innerHTML = `
+      <h1>Quiz Completed!</h1>
+      <p>Your score: ${score}/${quizData.length}</p>
+    `;
+    if (score == quizData.length){
+      quiz.innerHTML = `<p>anti fragile</p>`//password here
+    }
+    else{
+      quiz.innerHTML = `<p>You must not know me that well, you got some wrong, try again</p>`
+    }
+  }
+  
+  showQuestion();
